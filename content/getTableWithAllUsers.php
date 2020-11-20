9511b004e4cbdf44711bc5b81dcdbfbe836613d0
Blabolka <klochko.andrew.official@gmail.com>
2020-11-21 00:08:51 +0200

    First commit

---
+++ content/getTableWithAllUsers.php
@@ -0,0 +1,25 @@
+<table class="table">
+    <thead>
+        <th scope="col">ID</th>
+        <th scope="col">First Name</th> 
+        <th scope="col">Last Name</th>
+    </thead>
+    <tbody>
+        <?php
+            $db = require_once('../database/db.php');
+
+            $query = "SELECT id, firstName, lastName, id_role FROM users";
+            $result = mysqli_query($conn, $query);
+            if($result){
+                while($row = mysqli_fetch_array($result)){
+                    echo "<tr>";
+                    for($j=0 ; $j<4 ; $j++){
+                        echo "<td>$row[$j]</td>";
+                    }
+                    echo "</tr>";
+                    break;
+                }
+            }
+        ?>
+    </tbody> 
+</table>
---
+++ content/mainPageForUnauthorizedUser.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../boostrap/bootstrap.min.css">
+    </head>
+
+    <body>
+        <?php
+            require_once('./getTableWithAllUsers.php'); //print table with all users
+        ?>
+    </body>
+</html>

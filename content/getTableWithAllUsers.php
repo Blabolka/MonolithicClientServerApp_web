97de57dffd28fb14d183414a09decd39851b0502
Blabolka <bida.andrey01@gmail.com>
2020-11-21 23:42:39 +0200

    Updated project structure and added some useful buttons

--- content/getTableWithAllUsers.php
+++ content/getTableWithAllUsers.php
@@ -1,6 +1,6 @@
 <table class="table">
     <thead>
-        <th scope="col">ID</th>
+        <th scope="col">#</th>
         <th scope="col">First Name</th> 
         <th scope="col">Last Name</th>
         <th scope="col">Role</th>
--- content/mainPageForUnauthorizedUser.php
+++ content/mainPageForUnauthorizedUser.php
@@ -2,18 +2,29 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="../boostrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/boostrap/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/popUpLogin.css">
+    <link rel="stylesheet" href="../assets/css/horizontalCentralize.css">
 </head>
 
 <body>
-    <button type="button" class="btn btn-primary" id="signIn" onClick="PopUpShow()">SIGN IN</button>
-
+    
+    <div class="horizontalCentralizeContent">
+        <div class="buttons">
+            <button type="button" class="btn btn-primary" id="signIn" onClick="PopUpShow()">SIGN IN</button>
+            <button type="button" class="btn btn-primary" id="signUp" onClick="document.location='./registration.html'">SIGN UP</button>
+        </div>
+        
+        <?php
+            require_once('./getTableWithAllUsers.php'); //print table with all users
+        ?>
+    </div>
+    
     <div id="popUp">
         <div id="loginPopUp">
             
             <div class="form-group" id="closePopUp">
-                <button class="btn btn-secondary" onClick="PopUpHide()">X</button>
+                <button class="btn btn-danger" onClick="PopUpHide()">X</button>
             </div>
             <form action="" method="post">
                 <div class="form-group">
@@ -22,15 +33,15 @@
                 <div class="form-group">
                     <input type="password" class="form-control" name="password" placeholder="Password" minlength="6" required>
                 </div>
-                <button type="submit" class="btn btn-primary">SIGN IN</button>
+                <div class="buttons">
+                    <button type="submit" class="btn btn-primary">SIGN IN</button>
+                </div>
+                
             </form>
             
         </div>
     </div>
 
-    <?php
-        require_once('./getTableWithAllUsers.php'); //print table with all users
-    ?>
 </body>
 
 <script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
